2e7a4a74765dfbc29e41ef26f8f9d31d386f1bb5
Eniola Bakare <eebakare@gmail.com>
2022-12-25 00:27:36 +0100

    Added the functionality to the FAQ search input

--- src/components/redesignComponents/redesignFAQ.js
+++ src/components/redesignComponents/redesignFAQ.js
@@ -36,10 +36,11 @@ const redesignFAQ = () => {
     }
   ]
 
-
-
+  const [search, setSearch] = useState('')
   const [selected, setSelected] = useState(null);
+  console.log(search)
 
+ 
   const toggle = (i) => {
     if(selected === i){
       return setSelected(null)
@@ -54,7 +55,7 @@ const redesignFAQ = () => {
           <div className="faq-header-content d-flex flex-column align-items-center justify-contents-between">
             <h1 className='faq-title'>Frequently Asked Questions</h1>
             <div className="search-box-div ">
-              <input type='search' placeholder='search for any question' />
+              <input type='search' placeholder='search for any question' onChange={(e) => setSearch(e.target.value)}/>
               <span className="search d-flex justify-content-end align-items-center">
                 <i className="uil uil-search search-icon"></i>
               </span>
@@ -66,33 +67,37 @@ const redesignFAQ = () => {
 
         <div className="faq-body py-4 px-4 px-lg-5 py-lg-5 my-5">
           {
-            faqQuestions.map((eachQuestion, i) => 
-              (
-                <div className="each-faq-div py-3 py-lg-5 d-flex justify-content-between align-items-start border-bottom" key={eachQuestion.serialNumber}>
-                    <div className="question-number d-flex justify-content-start align-items-start">
-                      <h1 className='faq-number d-flex justify-content-start align-items-start'>{eachQuestion.serialNumber}</h1>
-                      <div className="question-answer-div pl-3 pl-md-5 d-flex flex-column justify-content-center align-items-start" >
-                        <h2 className='faq-question ' onClick={() => toggle(i)}>{eachQuestion.question}</h2>
-                        <h3 className={selected === i ? 'faq-answer show' : 'faq-answer '}>{eachQuestion.answer}</h3>
+              faqQuestions.filter(item => {
+                return(
+                  search.toLowerCase() ==='' ? item : item.question.toLowerCase().includes(search) || item.answer.toLowerCase().includes(search)
+                )
+              }).map((eachQuestion, i) => 
+                (
+                  <div className="each-faq-div py-3 py-lg-5 d-flex justify-content-between align-items-start border-bottom" key={eachQuestion.serialNumber}>
+                      <div className="question-number d-flex justify-content-start align-items-start">
+                        <h1 className='faq-number d-flex justify-content-start align-items-start'>{eachQuestion.serialNumber}</h1>
+                        <div className="question-answer-div pl-3 pl-md-5 d-flex flex-column justify-content-center align-items-start" >
+                          <h2 className='faq-question ' onClick={() => toggle(i)}>{eachQuestion.question}</h2>
+                          <h3 className={selected === i ? 'faq-answer show' : 'faq-answer '}>{eachQuestion.answer}</h3>
+                        </div>
                       </div>
-                    </div>
-                 
+                  
+                        
+                          {selected === i
+                          ?
+                            <div className="expand-div close-div d-flex align-items-center justify-content-center">
+                              <i className="uil uil-times close-icon" onClick={() => toggle(i)} ></i>
+                            </div>
+                          : 
+                            
+                            <div className="expand-div open-div d-flex align-items-center justify-content-center">
+                              <i className="uil uil-plus open-icon" onClick={() => toggle(i)}></i>
+                            </div>
+                          }
                       
-                        {selected === i
-                        ?
-                          <div className="expand-div close-div d-flex align-items-center justify-content-center">
-                            <i className="uil uil-times close-icon" onClick={() => toggle(i)} ></i>
-                          </div>
-                        : 
-                          
-                          <div className="expand-div open-div d-flex align-items-center justify-content-center">
-                            <i className="uil uil-plus open-icon" onClick={() => toggle(i)}></i>
-                          </div>
-                        }
-                    
-                </div>
+                  </div>
+                )
               )
-            )
           }
 
         </div>
